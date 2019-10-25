b407f7da1e961a7f7e7226244441d8d5b31ce234
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-10-25 15:57:59 +0300

    promo и site-header десктоп

--- source/js/common.js
+++ source/js/common.js
@@ -1,1 +1,18 @@
-console.log('hello')
+(function () {
+  var videoBackgroundElement = $('#promo');
+  var VIDEO_PATH = 'img/video/video';
+  var VIDEO_IMAGE = 'img/video/background-video.jpg';
+
+  videoBackgroundElement.vide({
+      mp4: VIDEO_PATH,
+      poster: VIDEO_IMAGE
+    },
+    {
+      loop: true,
+      autoplay: true,
+      position: '50% 50%',
+      resizing: true,
+      posterType: 'jpg',
+      className: 'promo__video'
+    });
+})();
