e31f8ca1589f6c8e6e9249d588a2f2971e11d56c
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-10-28 17:39:31 +0200

    reviews - десктоп

--- source/js/common.js
+++ source/js/common.js
@@ -3,6 +3,7 @@
   var VIDEO_PATH = 'img/video/video';
   var VIDEO_IMAGE = 'img/video/background-video.jpg';
   var employeesSlider = $('.best-employees__slider');
+  var reviewsSliders = $('.reviews__sliders');
 
   videoBackgroundElement.vide({
       mp4: VIDEO_PATH,
@@ -25,6 +26,41 @@
     slidesToScroll: 1,
     prevArrow: $('.best-employees__arrow--prev'),
     nextArrow: $('.best-employees__arrow--next'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  reviewsSliders.slick({
+    dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    prevArrow: $('.reviews__arrow--prev'),
+    nextArrow: $('.reviews__arrow--next'),
     responsive: [
       {
         breakpoint: 1024,
