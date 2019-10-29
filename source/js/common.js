f914355df542927932b7447939dbd0d1860e82b9
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-10-29 12:10:22 +0200

    Адаптив - до ipad-pro

--- source/js/common.js
+++ source/js/common.js
@@ -28,15 +28,6 @@
     nextArrow: $('.best-employees__arrow--next'),
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
@@ -62,15 +53,6 @@
     prevArrow: $('.reviews__arrow--prev'),
     nextArrow: $('.reviews__arrow--next'),
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
       {
         breakpoint: 600,
         settings: {
