d382bdb0f95284e15a659ab749f9b1febb4cc94a
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-10-29 15:45:30 +0200

    Адаптив Вертикальной ориентации мобильного устройства

--- source/js/common.js
+++ source/js/common.js
@@ -41,13 +41,6 @@
     nextArrow: $('.best-employees__arrow--next'),
     responsive: [
       {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
